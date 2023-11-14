27edc165dfcc10ba6c86a42e3a07a0921bc686de
oumaima <oumaimanaanaapro@gmail.com>
2023-11-14 18:20:15 +0100

    file containing functions

---
+++ 0-opcodes.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * add_to_stck- pushes an element to the stack
+ * 
+ * @stck: the doubly link list containing the stack
+ * 
+ * @linum: the line in the file
+*/
+stack_t add_to_stck(stack_t **stck, __attribute__((unused)) unsigned int linum)
+{
+    
+}
