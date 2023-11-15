4c66f6b70fae89e4f915e5e6685b09b28c38731d
Badr_Annabi <badrannabi@gmail.com>
2023-11-15 19:32:20 +0100

    add rotr prototype

--- monty.h
+++ monty.h
@@ -59,6 +59,7 @@ void _div(stack_t **stack, unsigned int line_number);
 void _mul(stack_t **stack, unsigned int line_number);
 void _pchar(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
 
 
 
