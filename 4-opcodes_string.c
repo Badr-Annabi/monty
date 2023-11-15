aa35a90fab8a8d48865165b79b5fc3185c42953c
Badr_Annabi <badrannabi@gmail.com>
2023-11-15 19:31:51 +0100

    add rotr

--- 4-opcodes_string.c
+++ 4-opcodes_string.c
@@ -15,7 +15,7 @@ void _pchar(stack_t **stack, unsigned int line_number)
 		print_error(16, line_number);
 	top_element = (*stack)->n;
 
-	if ((top_element < 0 || top_element > 127)
+	if (top_element < 0 || top_element > 127)
 		print_error(15, line_number);
 	printf("%c\n", top_element);
 }
@@ -44,3 +44,29 @@ void rotl(stack_t **stack, unsigned int line_number)
 	*stack = second_element;
 
 }
+/**
+ * rotr - rotates the stack to the bottom
+ * The last element of the stack becomes the first one,
+ * @stack: the stack
+ * @line_number: the line number of rotl opcode
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+        stack_t *iterator;
+	stack_t *last_elem;
+        (void) line_number;
+        if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+                return;
+        iterator = *stack;
+        while (iterator->next != NULL)
+                iterator = iterator->next;
+        last_elem = iterator;
+	iterator->prev->next = NULL;
+        last_elem->prev = NULL;
+	last_elem->next = *stack;
+        (*stack)->prev = last_elem;
+        *stack = last_elem;
+
+}
+
+
