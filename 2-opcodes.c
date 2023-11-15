3a475b23c892aaa8cc233a6d7487c856382879ce
Badr_Annabi <badrannabi@gmail.com>
2023-11-15 15:46:47 +0100

    add mod function

--- 2-opcodes.c
+++ 2-opcodes.c
@@ -61,3 +61,29 @@ void _mul(stack_t **stack, unsigned int line_number)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+
+/**
+ * _mod - computes the rest of the division
+ * of the second top element of the stack
+ * by the top element of the stack.
+ *
+ * @stack: the stack
+ *
+ * @line_number: the line where div is given
+ */
+void _mod(stack_t **stack, unsigned int line_number)
+{
+	int result, top_element;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		print_error(14, line_number);
+	(*stack) = (*stack)->next;
+	top_element = (*stack)->prev->n;
+	if (top_element == 0)
+		print_error(14, line_number);
+	result = (*stack)->n % top_element;
+	(*stack)->n = result;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
